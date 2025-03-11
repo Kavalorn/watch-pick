ce1273e76b6c531dede4264165f552b885c0d41b
Kavalorn <kavalorn12345@gmail.com>
2025-03-11 15:14:00 +0200

    fix

--- src/server.ts
+++ src/server.ts
@@ -241,7 +241,7 @@ app.get('/api/person/:id/credits', async (c) => {
 });
 
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 console.log(`Server running at http://localhost:${port}`);
 
 export default {
