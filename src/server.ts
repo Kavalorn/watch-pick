0ce6e70b69eea3bcb3846f2c7602d0baff4f67ec
Kavalorn <kavalorn12345@gmail.com>
2025-03-11 21:15:42 +0200

    test

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import * as schema from './schema';
 import 'dotenv/config';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
+
 // Initialize Supabase client
 const supabaseUrl = process.env.SUPABASE_AUTH_URL as string;
 const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
